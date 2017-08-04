aaf3b543bc509fb0d9cc804879656d5f2438b747
AlexWalker28 <alexwolk28@gmail.com>
2017-08-04 21:15:19 +0600

    Only people from the same group can see their events

--- app/src/main/java/kg/kloop/android/redbutton/EventsListViewAdapter.java
+++ app/src/main/java/kg/kloop/android/redbutton/EventsListViewAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -27,10 +29,13 @@ import java.util.Locale;
 
 class EventsListViewAdapter extends ArrayAdapter<Event> {
 
-    ArrayList eventsArrayList;
+    private Event currentEvent;
+    private FirebaseDatabase firebaseDatabase ;
+    private DatabaseReference databaseReference;
+    private TextView messageTextView;
+
     public EventsListViewAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull ArrayList<Event> events) {
         super(context, resource, events);
-        //eventsArrayList = events;
     }
 
     @NonNull
@@ -42,7 +47,9 @@ class EventsListViewAdapter extends ArrayAdapter<Event> {
                     R.layout.events_listview_item, parent, false);
         }
 
-        Event currentEvent = getItem(position);
+        currentEvent = getItem(super.getCount() - position - 1);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Users").child(currentEvent.getUser().getUserID()).child("message");
 
         TextView userNameTextView = (TextView) listItemView.findViewById(R.id.userNameTextView);
         userNameTextView.setText(currentEvent.getUser().getUserName());
@@ -52,6 +59,22 @@ class EventsListViewAdapter extends ArrayAdapter<Event> {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
         time = dateFormat.format(currentEvent.getTimeInMillis());
         timeTextView.setText(time);
+
+        messageTextView = (TextView)listItemView.findViewById(R.id.messageTextView);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String message = dataSnapshot.getValue(String.class);
+                messageTextView.setText(message);
+                Log.v("Message", "message: " + message);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
 
 
--- app/src/main/java/kg/kloop/android/redbutton/EventsTab.java
+++ app/src/main/java/kg/kloop/android/redbutton/EventsTab.java
@@ -26,8 +26,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Comparator;
+
+import kg.kloop.android.redbutton.groups.GroupRoom;
 
 /**
  * Created by alexwalker on 01.08.17.
@@ -42,17 +45,23 @@ public class EventsTab extends Fragment {
     EventsListViewAdapter adapter;
     ViewPager viewPager;
     SharedPreferences preferences;
+    DatabaseReference groupsDatabaseReference;
+    ArrayList<String> groupNamesArrayList;
+    GroupRoom groupRoom;
+    ArrayList<GroupRoom> groupRoomArrayList;
+    String currentUser;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.fragment_tab2_events, container, false);
         init();
 
-        databaseReference.addChildEventListener(new ChildEventListener() {
+        groupsDatabaseReference.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                eventsArrayList.add(dataSnapshot.getValue(Event.class));
-                adapter.notifyDataSetChanged();
+                groupNamesArrayList.add(dataSnapshot.getKey());
+                groupRoom = dataSnapshot.getValue(GroupRoom.class);
+                groupRoomArrayList.add(groupRoom);
             }
 
             @Override
@@ -75,13 +84,55 @@ public class EventsTab extends Fragment {
 
             }
         });
+
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Event event = dataSnapshot.getValue(Event.class);
+                String userID = event.getUser().getUserID();
+                Log.v("EventsTab", "current user: " + currentUser + "\n" +
+                                    "event user: " + event.getUser().getUserName() + "\n" +
+                                    "event userID: " + event.getUser().getUserID());
+                for (GroupRoom room : groupRoomArrayList) {
+                    if (room.getMembers().containsKey(userID) && room.getMembers().containsKey(currentUser)) {
+                        eventsArrayList.add(event);
+                        adapter.notifyDataSetChanged();
+                        Log.v("usersToShow", "users: " + event.getUser().getUserName() + "\n" +
+                                            "");
+                    }
+                }
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
         adapter = new EventsListViewAdapter(getContext(), R.layout.events_listview_item, eventsArrayList);
         eventsListView.setAdapter(adapter);
         eventsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 SharedPreferences.Editor editor = preferences.edit();
-                editor.putInt(Constants.EVENT_INDEX, position);
+                editor.putInt(Constants.EVENT_INDEX, Math.abs(eventsListView.getCount() - position - 1));
                 editor.apply();
                 Log.v("EventsTab", "saved index: " + preferences.getInt(Constants.EVENT_INDEX, 0));
                 viewPager.setCurrentItem(0, true);
@@ -95,9 +146,14 @@ public class EventsTab extends Fragment {
         eventsListView = (ListView)v.findViewById(R.id.eventsListView);
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference("Events");
+        groupsDatabaseReference = firebaseDatabase.getReference("Groups");
         eventsArrayList = new ArrayList<Event>();
         viewPager = (ViewPager) getActivity().findViewById(R.id.mapsViewPager);
         preferences = getActivity().getSharedPreferences(Constants.SHARED_PREF_FILE, Context.MODE_PRIVATE);
+        groupNamesArrayList = new ArrayList<>();
+        groupRoom = new GroupRoom();
+        groupRoomArrayList = new ArrayList<>();
+        currentUser = preferences.getString(Constants.CURRENT_USER_ID, "");
     }
 
 }
