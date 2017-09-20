8f095d43f85ca700bf050044495245aa78fa2726
AlexWalker28 <alexwolk28@gmail.com>
2017-09-20 20:06:25 +0600

    Implement webview

--- app/src/main/java/kg/kloop/android/redbutton/information/WebViewFragment.java
+++ app/src/main/java/kg/kloop/android/redbutton/information/WebViewFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.alexwalker.sendsmsapp.R;
 
@@ -25,6 +27,15 @@ public class WebViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_web_view, container, false);
+
+        WebView webView = (WebView)view.findViewById(R.id.info_web_view);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        // Configure the client to use when opening URLs
+        webView.setWebViewClient(new WebViewClient());
+        // Load the initial URL
+        webView.loadUrl("https://www.google.com");
 
 
 
