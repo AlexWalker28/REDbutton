31762e0bb9d30bdef0b613366a2b0f633d7315c8
AlexWalker28 <alexwolk28@gmail.com>
2017-10-08 21:24:36 +0600

    Add swipeRefreshLayout to rss feed item web view

--- app/src/main/java/kg/kloop/android/redbutton/information/InfoWebViewFragment.java
+++ app/src/main/java/kg/kloop/android/redbutton/information/InfoWebViewFragment.java
@@ -3,9 +3,11 @@ package kg.kloop.android.redbutton.information;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -17,6 +19,8 @@ import com.example.alexwalker.sendsmsapp.R;
  */
 public class InfoWebViewFragment extends Fragment {
     String url = "";
+    SwipeRefreshLayout swipeRefreshLayout;
+    WebView webView;
 
     public InfoWebViewFragment() {
         // Required empty public constructor
@@ -28,16 +32,32 @@ public class InfoWebViewFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_web_view, container, false);
 
-        WebView webView = (WebView)view.findViewById(R.id.info_web_view);
+        webView = (WebView)view.findViewById(R.id.info_web_view);
+        swipeRefreshLayout = (SwipeRefreshLayout)view.findViewById(R.id.rss_feed_item_swipe_refresh_layout);
         webView.getSettings().setLoadsImagesAutomatically(true);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
         // Configure the client to use when opening URLs
-        webView.setWebViewClient(new WebViewClient());
+        //webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if(newProgress == 100){
+                    swipeRefreshLayout.setRefreshing(false);
+                } else swipeRefreshLayout.setRefreshing(true);
+            }
+        });
         // Load the initial URL
         if(url.length() != 0){
             webView.loadUrl(url);
         } else webView.loadUrl("http://sozsende.info/pages");
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                webView.reload();
+            }
+        });
 
         return view;
     }
